08ab49cbb13a70e977f7778f68f895cd5b9b8a3e
Pawel Konior <pawel.konior@gmail.com>
2024-09-01 16:18:24 +0200

    model for contact form, add form to Django Admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message[:10] + ('...' if len(self.message) > 10 else '')
