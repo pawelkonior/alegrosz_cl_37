adfe2e1b5daa7f2bbb93926ce27bc33ee872c17a
Pawel Konior <pawel.konior@gmail.com>
2024-09-01 17:07:51 +0200

    setup django templates with basic configuration

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, 'contact/contact.html')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>Alegrosz</h1>')
+    return render(request, 'home/home.html')
